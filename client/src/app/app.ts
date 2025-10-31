f349c5097921e6b9333a9d958de8721a6ac896b5
iagodizlla <iagoschlemper@gmail.com>
2025-10-30 23:37:22 -0300

    Implementado componente shell

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ShellComponent } from "./components/shell/shell.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, ShellComponent],
 })
 export class App {}

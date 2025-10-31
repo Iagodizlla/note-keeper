f1fa8c28bc306aef77c933b5115a1f1ae116c72e
iagodizlla <iagoschlemper@gmail.com>
2025-10-31 14:39:51 -0300

    Implementado componente de Exclusão de Categoria

--- client/src/app/components/categorias/categoria.routes.ts
+++ client/src/app/components/categorias/categoria.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { ListarCategorias } from './listar/listar-categorias';
 import { CadastrarCategoria } from './cadastrar/cadastrar-categoria';
+import { ExcluirCategoria } from './excluir/excluir-categoria';
 import { EditarCategoria } from './editar/editar-categoria';
 
 export const categoriaRoutes: Routes = [
   { path: '', component: ListarCategorias },
   { path: 'cadastrar', component: CadastrarCategoria },
   { path: 'editar/:id', component: EditarCategoria },
+  { path: 'excluir/:id', component: ExcluirCategoria },
 ];

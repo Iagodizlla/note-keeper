d09cbac2d34ad1c265b34b5af9bf04ed03a658ba
iagodizlla <iagoschlemper@gmail.com>
2025-10-31 00:12:24 -0300

    Adicionada página inicial

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { ShellComponent } from "./components/shell/shell.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [MatButtonModule, MatIconModule, ShellComponent],
+  imports: [ShellComponent, RouterOutlet],
 })
 export class App {}

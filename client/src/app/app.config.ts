3e5451bef1ef8d9feb5b3a8b168786028ff75cef
iagodizlla <iagoschlemper@gmail.com>
2025-10-31 18:26:33 -0300

    Adicionados headers de autorização nos serviços

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,28 +1,53 @@
+import { map } from 'rxjs';
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, Routes } from '@angular/router';
+import { inject, ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { CanActivateFn, provideRouter, Router, Routes } from '@angular/router';
 
 import { provideNotifications } from './components/shared/notificacao/notificacao.provider';
+import { AuthService } from './components/auth/auth.service';
 import { provideAuth } from './components/auth/auth.provider';
+
+// retorna verdadeiro quando EXISTE um usuário autenticado
+const usuarioDesconhecidoGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.accessToken$.pipe(
+    map((token) => (!token ? true : router.createUrlTree(['/inicio']))),
+  );
+};
+
+const usuarioAutenticadoGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.accessToken$.pipe(
+    map((token) => (token ? true : router.createUrlTree(['/auth/login']))),
+  );
+};
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () => import('./components/auth/auth.routes').then((r) => r.authRoutes),
+    canActivate: [usuarioDesconhecidoGuard],
   },
   {
     path: 'inicio',
     loadComponent: () => import('./components/inicio/inicio').then((c) => c.Inicio),
+    canActivate: [usuarioAutenticadoGuard],
   },
   {
     path: 'categorias',
     loadChildren: () =>
       import('./components/categorias/categoria.routes').then((r) => r.categoriaRoutes),
+    canActivate: [usuarioAutenticadoGuard],
   },
   {
     path: 'notas',
     loadChildren: () => import('./components/notas/nota.routes').then((r) => r.notaRoutes),
+    canActivate: [usuarioAutenticadoGuard],
   },
 ];
 
